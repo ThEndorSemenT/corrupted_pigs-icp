4f8d6307ee509b0fb89d8e93c796cfe060d3f5b0
Henrique Costa <hmcosta2002@gmail.com>
2024-03-15 15:56:14 +0000

    navbar in progress

--- src/corrupted_pigs_frontend/src/App.jsx
+++ src/corrupted_pigs_frontend/src/App.jsx
@@ -3,25 +3,27 @@ import { corrupted_pigs_backend } from 'declarations/corrupted_pigs_backend';
 import Game from "./components/Game";
 import CreateGame from './components/CreateGame';
 import JoinGame from './components/JoinGame';
+import Navbar from './components/Navbar';
+import { Container } from '@chakra-ui/react';
 
 function App() {
   const [roomId, setRoomId] = useState(null);
   const [player1Cards, setPlayer1Cards] = useState([1, 10, 3]); // Sample cards for player 1
   const [player2Cards, setPlayer2Cards] = useState([4, 5, 6]); // Sample cards for player 2
-
-
-
   return (
     <div>
-      {!roomId ? (
-        <div>
-          <h1>Corrupted Pigs</h1>
-          <CreateGame />
-          <JoinGame />
-        </div>
-      ) : (
-        <Game player1Cards={player1Cards} player2Cards={player2Cards} />
-      )}
+      <Navbar />
+      <Container>
+        {!roomId ? (
+          <div>
+            <h1>Corrupted Pigs</h1>
+            <CreateGame />
+            <JoinGame />
+          </div>
+        ) : (
+          <Game player1Cards={player1Cards} player2Cards={player2Cards} />
+          )}
+      </Container>
     </div>
   );
 };
